224f0c9ee933ade75f39e483aa81cdb336dca455
miguel gomes <miguelg2001@hotmail.com>
2020-04-15 21:25:29 +0100

    1ª versao do projeto

---
+++ Gestão de Frota de Automóveis/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Gestão_de_Frota_de_Automóveis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gestão_de_Frota_de_Automóveis.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GridUtilizador()
+        {
+            Gestão_de_Frota_de_AutomoveisEntities db = new Gestão_de_Frota_de_AutomoveisEntities();
+            var result = db.Utilizador.ToList();
+            return View(result);
+        }
+        
+    }
+}
---
+++ Gestão de Frota de Automóveis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gestão_de_Frota_de_Automóveis.MvcApplication" Language="C#" %>
